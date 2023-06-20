fab4170e8b47adb1653fcb9e164081b7bf55b582
Huan Mai <huanmai101@gmail.com>
2023-06-20 15:58:50 -0400

    Attempted OctalStringToInt

--- hw05/octalStringToInt.asm
+++ hw05/octalStringToInt.asm
@@ -12,12 +12,21 @@
 ;;  int length = 4;
 ;;  int value = 0;
 ;;  int i = 0;
-;;  while (i < length) { ;; while (length - i > 0)
+;;  while (i < length) { ;; while (i - length < 0)
+
+
 ;;      int leftShifts = 3;
 ;;      while (leftShifts > 0) {
 ;;          value += value;
 ;;          leftShifts--;
-;;      }
+
+
+;;      } // MANUAL BLOCK
+;;      value += value;
+;;      value += value;
+;;      value += value; // END MANUAL
+
+
 ;;      int digit = octalString[i] - 48;
 ;;      value += digit;
 ;;      i++;
@@ -26,13 +35,23 @@
 
 .orig x3000
     ;; YOUR CODE HERE
-    HALT 
+    ;; R0 = answer
+    ;; R1 = current char
+
+    ;; R4 = temp register for storing/reading from memory
+    
+    HALT
+
 ;; Do not change these values! 
 ;; Notice we wrote some values in hex this time. Maybe those values should be treated as addresses?
 ASCII           .fill -48
 OCTALSTRING     .fill x5000
 LENGTH          .fill 4
 RESULTADDR      .fill x4000
+
+CURRENT_CHAR    .blkw 1
+NEG_LENGTH      .blkw 1
+;;
 .end
 
 .orig x5000                    ;;  Don't change the .orig statement
