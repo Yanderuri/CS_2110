4cdf60ef24fcc01fc97d54257058b0f628412c77
Huan Mai <huanmai101@gmail.com>
2023-07-07 14:57:21 -0400

    attempted to write my_strncmp

--- hw07/my_string.c
+++ hw07/my_string.c
@@ -54,7 +54,24 @@ int my_strncmp(const char *s1, const char *s2, size_t n)
     UNUSED_PARAM(s1);
     UNUSED_PARAM(s2);
     UNUSED_PARAM(n);
-
+    if (s1 == s2){
+        return 0;
+    }
+    long unsigned int i = 0;
+    while (i < n){
+        if (*s1 < *s2){
+           return -1; 
+        }
+        if (*s1 > *s2){
+            return 1;
+        }
+        if (*s1 == *s2 && *s1 == 0){
+            return 0;
+        }
+        s1 += sizeof(char);
+        s2 += sizeof(char);
+        i++;
+    }
     return 0;
 }
 
@@ -103,8 +120,7 @@ char *my_strncat(char *dest, const char *src, size_t n)
     UNUSED_PARAM(dest);
     UNUSED_PARAM(src);
     UNUSED_PARAM(n);
-    
-    return NULL;
+    return dest;
 }
 
 /**
